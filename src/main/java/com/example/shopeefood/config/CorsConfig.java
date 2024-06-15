0b0df9f892ef5ab298db1f4c4e6b729c39b2e298
nguyenductuyen18 <114349670+nguyenductuyen18@users.noreply.github.com>
2024-06-15 14:14:28 +0700

    Update CorsConfig.java

--- src/main/java/com/example/shopeefood/config/CorsConfig.java
+++ src/main/java/com/example/shopeefood/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
